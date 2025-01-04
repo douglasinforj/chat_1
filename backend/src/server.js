a53fffed48a8d901a712881bbc9273986eeab6f9
Douglas Silva <douglasitpro@gmail.com>
2025-01-04 16:28:59 -0300

    Adicionando envio de mensagem do servidor

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,6 +12,9 @@ wss.on("connection", (ws)=>{        //ws cliente conectando servidor
 
     ws.on("error", console.error)   //caso de algum erro
 
+    //teste de envio de mensagem do servidor ao cliente
+    ws.send("Mensagem enviada pelo servidor!")
+
     ws.on("message", (data)=>{          //disparado sempre que alguem enviar uma mensagem para o servidor, data = dados do cliente
         wss.clients.forEach((client) => client.send(data.toString()))     //Enviando mensagens para todos os clients conectados
     })
