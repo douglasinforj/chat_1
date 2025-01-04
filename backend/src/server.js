e4774bf6bf329a2a3712928bf175b31002bd469b
Douglas Silva <douglasitpro@gmail.com>
2025-01-04 18:22:29 -0300

    Verificando se o servidor esta recebendo mensagem do frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv")            //lidando com variaveis de ambiente
 
 dotenv.config()
 
-const wss = new WebSocketServer({ port: process.env.PORT || 8080})    //variavel PORT vem do arquivo .env, ou um valor alternativo 8080
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 })    //variavel PORT vem do arquivo .env, ou um valor alternativo 8080
 
 
 //criando um evento de conexão
@@ -12,12 +12,18 @@ wss.on("connection", (ws)=>{        //ws cliente conectando servidor
 
     ws.on("error", console.error)   //caso de algum erro
 
-    //teste de envio de mensagem do servidor ao cliente
+    //teste de envio de mensagem do servidor ao cliente que se conectou
     ws.send("Mensagem enviada pelo servidor!")
 
+    
     ws.on("message", (data)=>{          //disparado sempre que alguem enviar uma mensagem para o servidor, data = dados do cliente
+        //TODO:retirar
+        console.log(data.toString())
+
+        //Envia mensagens para todos
         wss.clients.forEach((client) => client.send(data.toString()))     //Enviando mensagens para todos os clients conectados
     })
 
     console.log("client connected")
+ 
 })
